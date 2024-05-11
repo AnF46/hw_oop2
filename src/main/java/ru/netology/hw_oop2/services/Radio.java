c953fdce6ea04a4120e4cce4da4d108d628c34e5
AnF <anfwork007@gmail.com>
2024-05-11 14:56:27 +0300

    initial commit

---
+++ src/main/java/ru/netology/hw_oop2/services/Radio.java
@@ -0,0 +1,71 @@
+package ru.netology.hw_oop2.services;
+public class Radio {
+    //stations
+    private int currentStation;
+    public int getCurrentStation() {
+        return currentStation;
+    };
+
+    public void setCurrentStation(int newCurrentStation) {
+        if (newCurrentStation > 9) {
+            return;
+        }
+        if (newCurrentStation < 0) {
+            return;
+        }
+        currentStation = newCurrentStation;
+    }
+
+    public void nextStation() {
+        int newsStation = currentStation + 1;
+        if (currentStation < 9) {
+            setCurrentStation(newsStation);
+        } else {
+            currentStation = 0;
+        }
+    }
+
+    public void prevStation() {
+        int prevStation = currentStation - 1;
+        if (currentStation > 0) {
+            setCurrentStation(prevStation);
+        } else {
+            currentStation = 9;
+        }
+    }
+
+    //volume
+    private int currentVolume;
+
+    public int getCurrentVolume() {
+        return currentVolume;
+    }
+
+    public void setCurrentVolume(int newCurrentVolume) {
+        if (newCurrentVolume > 100) {
+            return;
+        }
+        if (newCurrentVolume < 0) {
+            return;
+        }
+        currentVolume = newCurrentVolume;
+    }
+
+    public void increaseVolume() {
+        int moreVolume = currentVolume + 1;
+        if (currentVolume < 100) {
+            setCurrentVolume(moreVolume);
+        } else {
+            getCurrentVolume();
+        }
+    }
+
+    public void decreaseVolume() {
+        int lessVolume = currentVolume - 1;
+            if (currentVolume > 0) {
+                setCurrentVolume(lessVolume);
+            } else {
+                getCurrentVolume();
+            }
+    }
+}
---
+++ src/test/java/ru/netology/hw_oop2/services/RadioTest.java
@@ -0,0 +1,121 @@
+package ru.netology.hw_oop2.services;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class RadioTest {
+    Radio station = new Radio();
+    Radio volume = new Radio();
+    @Test
+    public void shouldSetNewStation() {
+
+        station.setCurrentStation(3);
+
+        int expected = 3;
+        int actual = station.getCurrentStation();
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldSetZeroAfterNine() {
+
+        station.setCurrentStation(9);
+        station.nextStation();
+
+        int expected = 0;
+        int actual = station.getCurrentStation();
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldNotSetAboveMaxStation() {
+         station.setCurrentStation(10);
+
+         int expected = 0;
+         int actual = station.getCurrentStation();
+
+         Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldNotSetLessMinStation() {
+        station.setCurrentStation(9);
+        station.prevStation();
+
+        int expected = 8;
+        int actual = station.getCurrentStation();
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldSetNextStation() {
+        station.setCurrentStation(7);
+        station.nextStation();
+
+        int expected = 8;
+        int actual = station.getCurrentStation();
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldSetNineAfterZero() {
+        station.setCurrentStation(0);
+        station.prevStation();
+
+        int expected = 9;
+        int actual = station.getCurrentStation();
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    //VOLUME TESTS
+
+    @Test
+    public void shouldASetVolume() {
+
+        volume.setCurrentVolume(50);
+
+        int expected = 50;
+        int actual = volume.getCurrentVolume();
+
+        Assertions.assertEquals(expected, actual);
+
+    }
+
+    @Test
+    public void shouldIncreaseVolume() {
+        volume.setCurrentVolume(60);
+        volume.increaseVolume();
+
+        int expected = 61;
+        int actual = volume.getCurrentVolume();
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldNotSetAboveMaxVolume() {
+        volume.setCurrentVolume(100);
+        volume.increaseVolume();
+
+        int expected = 100;
+        int actual = volume.getCurrentVolume();
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldNotSetBelowMinVolume() {
+        volume.setCurrentVolume(0);
+        volume.decreaseVolume();
+
+        int expected = 0;
+        int actual = volume.getCurrentVolume();
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+}
